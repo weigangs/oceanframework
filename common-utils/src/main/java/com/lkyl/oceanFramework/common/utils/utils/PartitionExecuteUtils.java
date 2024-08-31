25a6c90c548e0803c010fb687fc789d049c439d8
nicholas <18710329690@163.com>
2024-08-31 21:20:42 +0800

    add new feature

--- common-utils/src/main/java/com/lkyl/oceanFramework/common/utils/utils/PartitionExecuteUtils.java
+++ common-utils/src/main/java/com/lkyl/oceanFramework/common/utils/utils/PartitionExecuteUtils.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.ToIntFunction;
 
 public class PartitionExecuteUtils {
     public static <T, R> List<R> queryDbPartition(
@@ -25,5 +26,22 @@ public class PartitionExecuteUtils {
             resultList.addAll(executeFunc.apply(part));
         });
         return resultList;
+    }
+
+    public static <T> int updateDbPartition(
+            List<T> allList, ToIntFunction<List<T>> executeFunc,
+            int partitionSize
+    ) {
+        if (CollectionUtils.isEmpty(allList)) {
+            return 0;
+        }
+        if (partitionSize <= 0) {
+            partitionSize = allList.size();
+        }
+        int resultCount = 0;
+        for (List<T> part : Lists.partition(allList, partitionSize)) {
+            resultCount += executeFunc.applyAsInt(part);
+        }
+        return resultCount;
     }
 }
